277e03b87720566dc9228a3f2c5464c90716c28f
CL2228 <cl2228@cornell.edu>
2022-04-09 16:37:06 -0400

    Test azure face.

--- azure-test/fundamental-tests/face_test.py
+++ azure-test/fundamental-tests/face_test.py
@@ -20,10 +20,11 @@ from blob_test import *
 CONTAINER_NAME = "test-container"
 BLOB_NAME = "ray-img"
 
-FACE_KEY = "c5f7202b0f774f349c7e85f576c88230"
-FACE_ENDPOINT = "https://final-project-face.cognitiveservices.azure.com/"
+FACE_KEY = "f15b29c9e66b461da5edf600cf819b06"
+FACE_ENDPOINT = "https://cs5412-final-project-face-paid.cognitiveservices.azure.com/"
 
 face_client = FaceClient(FACE_ENDPOINT, CognitiveServicesCredentials(FACE_KEY))
+
 
 # Convert width height to a point in a rectangle
 def getRectangle(faceDictionary):
@@ -32,14 +33,15 @@ def getRectangle(faceDictionary):
     top = rect.top
     right = left + rect.width
     bottom = top + rect.height
-    
+
     return ((left, top), (right, bottom))
 
-def drawFaceRectangles(bytes_data, detected_faces) :
-# Download the image from the url
+
+def drawFaceRectangles(bytes_data, detected_faces):
+    # Download the image from the url
     img = Image.open(bytes_data)
 
-# For each face returned use the face rectangle and draw a red box.
+    # For each face returned use the face rectangle and draw a red box.
     print('Drawing rectangle around face... see popup for results.')
     draw = ImageDraw.Draw(img)
     for face in detected_faces:
@@ -49,10 +51,10 @@ def drawFaceRectangles(bytes_data, detected_faces) :
 
 if __name__ == "__main__":
     print("running")
-    raw_bytes = read_a_blob(CONTAINER_NAME, BLOB_NAME)
-    img_file = open("./sample.jpg", "rb")
-    io_bytes = BytesIO(raw_bytes)
-    print(face_client)
+    # raw_bytes = read_a_blob(CONTAINER_NAME, BLOB_NAME)
+    # img_file = open("./sample.jpg", "rb")
+    # io_bytes = BytesIO(raw_bytes)
+    # print(face_client)
 
     # face_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
     # detected_faces = face_client.face.detect_with_stream(image=io_bytes)
@@ -60,18 +62,13 @@ if __name__ == "__main__":
     #     raise Exception("No face detected")
     # for face in detected_faces:
     #     print('Detected face ID:  {}'.format(face.face_id))
-    
+
     # drawFaceRectangles(io_bytes, detected_faces)
 
-    verify_result_diff = face_client.face.verify_face_to_face("7474dbb4-bd4f-42ff-8679-82fd4e09b172",
-    "7474dbb4-bd4f-42ff-8679-82fd4e09b172")
+    verify_result_diff = face_client.face.verify_face_to_face("f92c9821-fa9a-44c1-80b1-d5aaff4e9976",
+                                                              "f92c9821-fa9a-44c1-80b1-d5aaff4e9976")
     print('Faces from {} & {} are of the same person, with confidence: {}'
-    .format("face1", "face2", verify_result_diff.confidence)
-    if verify_result_diff.is_identical
-    else 'Faces from {} & {} are of a different person, with confidence: {}'
-        .format("face1", "face2", verify_result_diff.confidence))
-
-
-
-
-
+          .format("face1", "face2", verify_result_diff.confidence)
+          if verify_result_diff.is_identical
+          else 'Faces from {} & {} are of a different person, with confidence: {}'
+          .format("face1", "face2", verify_result_diff.confidence))
