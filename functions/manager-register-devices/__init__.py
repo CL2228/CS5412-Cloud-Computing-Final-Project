1016c2f4ba81f524d54f83eb4b2f37fc9a6c80cf
CL2228 <cl2228@cornell.edu>
2022-04-08 23:02:00 -0400

    Modify manager-register-devices function
    Add "azure-iothub-device-id" as a property of "devices" collection in MongoDB

--- functions/manager-register-devices/__init__.py
+++ functions/manager-register-devices/__init__.py
@@ -1,5 +1,15 @@
 """
     Register a new IOT device for the system, and blind it with a unit
+
+
+    Trigger: HTTP Request
+    Output: HTTP Response
+
+    BODY: JSON
+    'device-key': the primary key of Azure IoT Hub devices
+    'device-id': the Device ID of Azure IoT Hub devices, usually means the name of that device
+    'unit_id': the unit ID, usually is the Object ID of Unit in MongoDB
+
 """
 import azure.functions as func
 import json
@@ -12,18 +22,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         req_body = req.get_json()
         device_key = req_body['azure-iothub-device-key']
+        device_id = req_body['azure-iothub-device-id']
         unit_id = req_body['unit-id']
+
 
         unit_found, _ = mongodb_utils.query_one("units", {'_id': unit_id})
         if not unit_found:
             res_body['message'] = "Unit not found, please check the ID again"
             return func.HttpResponse(json.dumps(res_body), headers=res_headers, status_code=404)
 
-        device_found, device_data = mongodb_utils.query_one("devices", {'device_key': device_key})
+        device_found, device_data = mongodb_utils.query_one("devices", {'device_key': device_key, 'device_id': device_id})
         if device_found:
             insert_status, _ = mongodb_utils.update_one("devices", device_data, {"unit_id": unit_id})
         else:
-            device_data = {"devices_key": device_key, "unit_id": unit_id}
+            device_data = {"devices_key": device_key, "unit_id": unit_id, "device_id": device_id}
             insert_status, _ = mongodb_utils.insert("devices", device_data)
         if not insert_status:
             res_body['message'] = "Internal errors. Please try again"
