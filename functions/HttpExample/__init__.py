015402ec9f383699e08e930161f534bb8aad60ed
CL2228 <cl2228@cornell.edu>
2022-04-09 11:30:41 -0400

    Modified test function

--- functions/HttpExample/__init__.py
+++ functions/HttpExample/__init__.py
@@ -5,31 +5,9 @@ import azure.functions as func
 from ..server_functions.blob import azure_blob_helpers
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-
-    # name = req.params.get('name')
-    # if not name:
-    #     try:
-    #         req_body = req.get_json()
-    #     except ValueError:
-    #         pass
-    #     else:
-    #         name = req_body.get('name')
-    #
-    # if name:
-    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    # else:
-    #     return func.HttpResponse(
-    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-    #          status_code=200
-    #     )
-
-    data_status, data = azure_blob_helpers.read_blob("test.jpg")
-    if not data_status:
-        return func.HttpResponse("Data not found", status_code=404)
-
-    header = {'Content-Type': 'image/jpeg'}
-    response = func.HttpResponse(data, status_code=200, headers=header)
+    msg.set("LCH Messge")
+    response = func.HttpResponse("message", status_code=200)
     return response
