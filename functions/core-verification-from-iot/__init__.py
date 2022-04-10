2d0c8f1b5c64424f863941d631d81418e15f1d01
CL2228 <cl2228@cornell.edu>
2022-04-09 22:47:57 -0400

    Implement core-verification-from-iot function
    Now achieved:
    1.triggered by eventhub and process the verification request
    2.verify faces by retrieving tenants' faces and guests' faces
    
    TODO:
    1.design the result process part

--- functions/core-verification-from-iot/__init__.py
+++ functions/core-verification-from-iot/__init__.py
@@ -1,12 +1,116 @@
 from typing import List
 import logging
-
 import azure.functions as func
+import pickle
+from ..server_functions.face import azure_face_helpers
+from ..server_functions.config import imgBaseUrl
+from ..server_functions.mongodb import mongodb_utils
+from ..server_functions.jwt import jwt_utils
+from ..server_functions.blob import azure_blob_helpers
+from io import BytesIO
 
 
 def main(events: List[func.EventHubEvent], msg: func.Out[func.QueueMessage]):
     for event in events:
-        # logging.info('Python EventHub trigger processed an event: %s',
-        #                 event.get_body().decode('utf-8'))
-        body = event.get_body().decode('utf-8')
-        msg.set(str(body))
+        body = dict(pickle.loads(event.get_body()))
+        logging.info("---------------------------------------")
+        logging.info('Python EventHub trigger processed an event: %s',
+                     body)
+        res_body = handle_one_event(event)
+
+        if type(res_body) == dict:
+            msg.set(res_body['verify_identity'])
+        logging.info("RESULT---------------------------------------")
+        logging.info(res_body)
+
+
+def handle_one_event(event: func.EventHubEvent):
+    """
+    handle an event in the event-hub "verification-request-event"
+
+    :param event: an EventHubEvent object
+    :return: a dict object that records the result (if there is no internal error)
+                otherwise return exceptions
+    """
+    res_body = {'timestamp': event.enqueued_time.timestamp(),
+                'unit_id': "",
+                'device_id': "",
+                'blob_name': "",
+                'verified': False,
+                "verify_identity": "",
+                "reference_img": None,
+                "confidence": 0.0}
+    try:
+        print("----------reach here")
+        # basic response body
+        body = dict(pickle.loads(event.get_body()))
+        res_body['unit_id'] = body['unit_id']
+        res_body['device_id'] = body['device_id']
+        res_body['blob_name'] = body['blob_name']
+        query_img_steam = BytesIO(azure_blob_helpers.read_blob(body['blob_name'])[1])
+
+        # get query face
+        query_face_detected, query_face_id = azure_face_helpers.get_faceId_with_stream(query_img_steam)
+        if not query_face_detected:
+            res_body['verify_identity'] = "Face not detected"
+            return res_body
+
+        print("----UnitID: {}".format(body['unit_id']))
+        print("----FaceID: {}".format(query_face_id))
+
+        # get unit information
+        unit_found, unit_data = mongodb_utils.query_one("units", {'_id': body['unit_id']})
+        if not unit_found:
+            res_body['verify_identity'] = "Unit not found"
+            return res_body
+
+        # get tenants associated with the unit and check if this person is a tenant
+        tenants = unit_data['tenants']
+        for tenant_email in tenants.keys():
+            tenant_found, tenant_data = mongodb_utils.query_one("tenants", {'email': tenant_email})
+            if not tenant_found or tenant_data['face_img'] is None:
+                continue
+            logging.info("----------tenant_img:{}".format(tenant_data['face_img']))
+            tenant_img_stream = BytesIO(azure_blob_helpers.read_blob(tenant_data['face_img'])[1])
+            tenant_face_detected, tenant_face_id = azure_face_helpers.get_faceId_with_stream(tenant_img_stream)
+            if not tenant_face_detected:
+                continue
+            verify_res, verify_confidence = azure_face_helpers.verify_two_faces(query_face_id, tenant_face_id)
+            if verify_res:
+                res_body['verified'] = True
+                res_body['confidence'] = verify_confidence
+                res_body['verify_identity'] = "tenant: {}".format(tenant_data['first_name'] + " " + tenant_data['last_name'])
+                res_body['reference_img'] = tenant_data['face_img']
+                return res_body
+
+        print("------Didn't find a tenant")
+
+        # check guests of this unit
+        guest_existed, guests = mongodb_utils.query_many("guests", {"unit": body['unit_id']})
+        print("-----Guest number: {}".format(len(guests)))
+        if guest_existed:
+            for guest in guests:
+                guest_token = guest['token']
+                guest_token_valid, guest_token_payload = jwt_utils.verify_jwt(guest_token)
+                if not guest_token_valid:
+                    continue
+                guest_face_id = guest_token_payload['face_id']
+                verify_res, verify_confidence = azure_face_helpers.verify_two_faces(query_face_id, guest_face_id)
+                if verify_res:
+                    res_body['verified'] = True
+                    res_body['confidence'] = verify_confidence
+                    res_body['verify_identity'] = "guest: {}".format(guest['first_name'] + " " + guest['last_name'])
+                    res_body['reference_img'] = guest_token_payload['face_img']
+                    return res_body
+
+        res_body['verify_identity'] = "Stranger"
+        return res_body
+    except KeyError:
+        return "illegal message"
+    except Exception as ex:
+        return ex
+
+
+def handle_result(res_body):
+    if type(res_body) != dict:
+        return False
