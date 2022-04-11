0c5645b3d234d9f6619bdccb9ce6d02c2b0bfd3c
CL2228 <cl2228@cornell.edu>
2022-04-10 20:07:32 -0400

    Modify Http Example to trigger events.

--- functions/HttpExample/__init__.py
+++ functions/HttpExample/__init__.py
@@ -3,11 +3,22 @@ import logging
 import azure.functions as func
 
 from ..server_functions.blob import azure_blob_helpers
+import datetime
+import pickle
+# from ..server_functions.eventhub import eventhub_utils
 
 
-def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> func.HttpResponse:
+def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage], eventHubOutput: func.Out[bytes]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    msg.set("LCH Messge")
+    time_now = datetime.datetime.now()
+
+    # eventHubOutput
+    event_body = {'device_id': "gates-hall-g01",
+                  'blob_name': "gates-hall-g01/records/b2be69f0-c3ff-4801-9fad-ef2eec87e52b.jpg",
+                  'unit_id': "62475aaadd78bdc4e2448eb8"}
+    
+    # eventHubOutput.set(pickle.dumps(event_body))
+    msg.set("Function trigger at {}".format(time_now.time()))
     response = func.HttpResponse("message", status_code=200)
     return response
