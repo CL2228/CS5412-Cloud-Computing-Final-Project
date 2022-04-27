b506199e746c53f4149255c766110b7cf7d2d1e8
CL2228 <cl2228@cornell.edu>
2022-04-27 14:33:08 -0400

    Redesign IoT event structure.

--- iot-hub/upload_img_script.py
+++ iot-hub/upload_img_script.py
@@ -16,23 +16,17 @@ PATH_OF_FILE = "./sample.jpg"
 DEVICE_KEY = "ETLt4rqzS+1Fo1tOfzq+sKfMoPaG1c8sI9+9UmW6QT4="
 
 
-async def upload_img_from_device(connection_string: str, file_path: str):
+async def upload_img_from_device(connection_string: str, device_key: str, file_path: str):
     device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 
     try:
         await device_client.connect()
-
-        # check whether the device is registered and get the unit id of this device
-        device_existed, device_data = mongodb_utils.query_one("devices", {"device_key": DEVICE_KEY})
-        if not device_existed:
-            return False, "Device not registered"
 
         # check image format and get suffix
         check_img, suffix = azure_blob_helpers.check_img_file_suffix(file_path)
         if not check_img:
             return False, "Not an image"
         # get the unit ID and generate unique file name
-        unit_id = device_data['unit_id']
         blob_name = azure_blob_helpers.generate_file_name(prefix="records/", suffix="." + suffix)
 
         print("reach here: {}".format(blob_name))
@@ -40,23 +34,23 @@ async def upload_img_from_device(connection_string: str, file_path: str):
         storage_info = await device_client.get_storage_info_for_blob(blob_name)
         print(storage_info['blobName'])
 
-        sas_url = "https://{}/{}/{}{}".format(
-            storage_info['hostName'],
-            storage_info['containerName'],
-            storage_info['blobName'],
-            storage_info['sasToken']
-        )
-        print(sas_url)
-
-        with BlobClient.from_blob_url(sas_url) as blob_client:
-            with open(file_path, "rb") as f:
-                print("saving")
-                result = blob_client.upload_blob(f, overwrite=True,
-                                                 content_settings=ContentSettings(content_type='image/jpeg'))
-                print(result)
-
-        event_payload = {"blob_name": storage_info['blobName'], "unit_id": unit_id, "device_id": device_data['device_id']}
-        await event_hub_helper.send_event("verification-request-event", event_payload)
+        # sas_url = "https://{}/{}/{}{}".format(
+        #     storage_info['hostName'],
+        #     storage_info['containerName'],
+        #     storage_info['blobName'],
+        #     storage_info['sasToken']
+        # )
+        # print(sas_url)
+        #
+        # with BlobClient.from_blob_url(sas_url) as blob_client:
+        #     with open(file_path, "rb") as f:
+        #         print("saving")
+        #         result = blob_client.upload_blob(f, overwrite=True,
+        #                                          content_settings=ContentSettings(content_type='image/jpeg'))
+        #         print(result)
+        #
+        # event_payload = {"blob_name": storage_info['blobName'], "device_key": device_key}
+        # await event_hub_helper.send_event("verification-request-event", event_payload)
         return True, "success"
     except FileNotFoundError:
         return False, "File Not Found."
@@ -68,4 +62,4 @@ async def upload_img_from_device(connection_string: str, file_path: str):
 
 
 if __name__ == "__main__":
-    print(asyncio.run(upload_img_from_device(DEVICE_CONN_STR, PATH_OF_FILE)))
+    print(asyncio.run(upload_img_from_device(DEVICE_CONN_STR, DEVICE_KEY, PATH_OF_FILE)))
