2ca82a292ca97703694030e2134ab5877143ff70
CL2228 <cl2228@cornell.edu>
2022-04-27 14:27:55 -0400

    Add event tester.

--- iot-hub/event_hub_helper.py
+++ iot-hub/event_hub_helper.py
@@ -27,11 +27,11 @@ async def send_event(event_hub_name: str, payload, conn_str: str = EVENT_HUB_STR
     except Exception as ex:
         return False, ex
 
-if __name__ == "__main__":
-    event_body = {'device_id': "gates-hall-g01",
-                  'blob_name': "gates-hall-g01/records/b2be69f0-c3ff-4801-9fad-ef2eec87e52b.jpg",
-                  'unit_id': "62475aaadd78bdc4e2448eb8"}
 
+if __name__ == "__main__":
+    # event_body = {'device_id': "gates-hall-g01",
+    #               'blob_name': "gates-hall-g01/records/b2be69f0-c3ff-4801-9fad-ef2eec87e52b.jpg",
+    #               'unit_id': "62475aaadd78bdc4e2448eb8"}
     res_event_body = {'timestamp': 1649651613.762,
                       'unit_id': '62475aaadd78bdc4e2448eb8',
                       'device_id': 'gates-hall-g01',
@@ -40,6 +40,13 @@ if __name__ == "__main__":
                       'verify_identity': 'Stranger',
                       'reference_img': None,
                       'confidence': 0.0}
+    tenant_blob = "tenants/6251ed05f429e641197c1a37/ebca445b-be87-407d-8267-5bdce0a65c95.png"
+    guest_blob = "gates-hall-g01/records/b2be69f0-c3ff-4801-9fad-ef2eec87e52b.jpg"
+    event_body = {"device_key": "ETLt4rqzS+1Fo1tOfzq+sKfMoPaG1c8sI9+9UmW6QT4=",
+                      "blob_name": tenant_blob}
     res = asyncio.run(send_event("verification-request-event", event_body))
     # res = asyncio.run(send_event("write-records-event", res_event_body))
+
+    # res = asyncio.run(send_event("test-event", event_body))
+    # res = asyncio.run(send_event("notify-iot-device-event", res_event_body))
     print(res)
